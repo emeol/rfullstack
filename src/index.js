0da684e57ba5127c5f1649a256ca2aea0e1445dc
meol <eumeol@gmail.com>
2018-10-28 23:36:05 +0000

    added basic hook

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App= ()=>{
+    return <h1>Hello Unboiler Plate!</h1>
+}
+
+const droot = document.getElementById('root');
+ReactDOM.render(<App/>, droot);
