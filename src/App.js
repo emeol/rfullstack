de6cbe1a00ba51a4ae00626ba81078ab3b56e27e
meol <eumeol@gmail.com>
2018-10-28 22:55:10 +0000

    initial commit - update index.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hello. This is a boiler plate template.
           </p>
           <a
             className="App-link"
